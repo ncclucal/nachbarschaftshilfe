2297f8a6a03c9caebfd2950a848c3935b4f1477d
bestsoft100 <bestsoft100@gmail.com>
2020-03-18 16:40:00 +0100

    Erste Datenbankfunktionalität

--- src/hammerhilfe/DatabaseHandler.java
+++ src/hammerhilfe/DatabaseHandler.java
@@ -1,21 +1,47 @@
 package hammerhilfe;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
 public class DatabaseHandler {
 	
-	private static DatabaseHandler instance = new DatabaseHandler();
+	private static DatabaseHandler instance = null;
 	
 	public static DatabaseHandler getInstance() {
 		return instance;
 	}
 	
+	public static void connect() {
+		try {
+			Connection connection = (Connection) DriverManager.getConnection("jdbc://bk-medienberufe.de/ita54_ilju","ita54_ilju","ABsuJ03a");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private Connection connection;
+	private Statement statement;
+	
+	public DatabaseHandler(Connection connection) {
+		this.connection = connection;
+		try {
+			this.statement = (Statement) connection.createStatement();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 	public int getInteger(String query) {
-		//TODO
-		//id
-		//email
-		//password
-		//username
-		//created date
-		return 0;
+		try {
+			ResultSet rs = statement.executeQuery(query);
+			return rs.getInt(1);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
